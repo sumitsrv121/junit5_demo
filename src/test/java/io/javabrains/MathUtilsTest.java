ad10787fcd846b65d0a0b585c98e86f1c9d4d250
sumit <sumitsaurav121@gmail.com>
2022-01-11 12:14:33 +0530

    updated with circle related area test

--- src/test/java/io/javabrains/MathUtilsTest.java
+++ src/test/java/io/javabrains/MathUtilsTest.java
@@ -7,10 +7,10 @@ class MathUtilsTest {
     @Test
     void testSumOfTwoPositiveNumbers() {
         MathUtils mathUtils = new MathUtils();
-        //input
+        // input
         int x = 10;
         int y = 20;
-        //actual output
+        // actual output
         Assertions.assertEquals(30, mathUtils.add(x, y), "The add method should add 2 numbers");
 
     }
